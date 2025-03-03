45085e0aadfdfb5ae2307157851f62399200c531
Carl <carlmesias20@gmail.com>
2025-03-02 20:57:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rlpi-basic-edi-system
+ Basic EDI ssytem, customer submits purchase order, supplier approves or denies. 
---
+++ back-end-python-endpoint/main.py
@@ -0,0 +1,18 @@
+# Basic EDI System by Carl Mesias
+# 
+# end-point that handles get requests from both 
+# supplier and customer front-end website
+from fastapi import FastAPI
+
+
+
+
+
+
+
+
+
+
+
+
+
